760d89e7bb4aa6859e44c33ec26a7f663e6125dc
Jean-Yves Lefort <jylefort@brutele.be>
2011-12-28 16:14:01 +0100

    Fix build.

--- src/mn-md5.c
+++ src/mn-md5.c
@@ -42,7 +42,7 @@
  * is a multiple of 4.
  */
 static void
-mn_md5_encode (unsigned char *output, u_int32_t input, unsigned int len)
+mn_md5_encode (unsigned char *output, u_int32_t *input, unsigned int len)
 {
   unsigned int i, j;
 
